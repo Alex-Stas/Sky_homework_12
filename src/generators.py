6129c4e591786929d7ecacc9f394fde976e95365
Alex-Stas <alexander.stashchuk@gmail.com>
2025-01-25 17:39:26 +0300

    Testing for all generators added, generators modified for incorrect data resolving

--- src/generators.py
+++ src/generators.py
@@ -1,19 +1,36 @@
 def filter_by_currency(list_of_transactions, currency):
+    """Функция фильтрует список по статусу и выдает новый список только с заданными
+        значениями, по умолчанию статус EXECUTED"""
     for i in list_of_transactions:
+        # пропуск транзакции, если в ней отсутствует запись о валюте
+        if 'operationAmount' not in i:
+            continue
+        if 'currency' not in i['operationAmount']:
+            continue
+        if 'code' not in i['operationAmount']['currency']:
+            continue
+
         if i['operationAmount']['currency']['code'] == currency:
             yield i
-    yield 'End of Data'
+    # Обработка случаев окончания или пустого списка, отсутствия записей заданной валюты
+    raise StopIteration('End of list or absent data')
 
 
 def transaction_descriptions(list_of_transactions):
     for i in list_of_transactions:
+        # пропуск транзакции, если в ней отсутствует описание операции
+        if 'description' not in i:
+            continue
         yield i['description']
-    yield 'End of Data'
+    # Обработка случаев окончания или пустого списка, отсутствия записей заданной валюты
+    raise StopIteration('End of list or absent data')
 
 
 
 
 def card_number_generator(start, stop):
+    if start > stop or isinstance(start, int) != True or isinstance(stop, int) != True or stop > 9999999999999999:
+        return 'Incorrect range'
     for i in range(start, stop + 1):
         i_str = str(i)
         block_1, block_2, block_3, block_4 = '0000', '0000', '0000', '0000'
