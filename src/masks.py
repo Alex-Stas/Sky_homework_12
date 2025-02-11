4a5e20b0c9ae49000b29aba23b859dc613d53afe
Alex-Stas <alexander.stashchuk@gmail.com>
2025-02-12 01:00:17 +0300

    Mmasks logging, linters, comments for teststing

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,25 @@
+import logging
+
+
+logging.basicConfig(
+    filename="../logs/masks.log",
+    filemode="w",
+    format="%(asctime)s:%(module)s:%(levelname)s:%(message)s",
+    level=logging.DEBUG,
+)
+
+logger = logging.getLogger()
+
+
 def get_mask_card_number(full_card_number: int) -> str:
     """
     Функция принимает на вход номер карты в виде числа и возвращает
     маску номера по правилу XXXX XX** **** XXXX
     """
+    logger.info("get_mask_card_number started")
     card_number_string = str(full_card_number)
     masked_card_number = f"{card_number_string[:4]} {card_number_string[4:6]}** **** {card_number_string[-4:]}"
+    logger.info("get_mask_card_number ended")
     return masked_card_number
 
 
@@ -13,28 +28,31 @@ def get_mask_account(full_account_number: int) -> str:
     Функция принимает на вход номер счета в виде числа и возвращает
     маску номера по правилу **XXXX
     """
+    logger.info("get_mask_account started")
     account_number_string = str(full_account_number)
     masked_account_number = f"**{account_number_string[-4:]}"
+    logger.info("get_mask_account ended")
     return masked_account_number
 
 
-if __name__ == "__main__":
-    account_numbers = [
-        1234567812345678,
-        10001111222233334444,
-        12345678901234567890,
-        1,
-        12,
-        123,
-        1234,
-        12345,
-        123456,
-        "",
-        "1234567890123456",
-        None,
-        True
-    ]
-    for i in account_numbers:
-        print(get_mask_account(i))
-    for i in account_numbers:
-        print(get_mask_card_number(i))
+# Тестовый блок, для запуска раскомментировать
+# if __name__ == "__main__":
+#     account_numbers = [
+#         1234567812345678,
+#         10001111222233334444,
+#         12345678901234567890,
+#         1,
+#         12,
+#         123,
+#         1234,
+#         12345,
+#         123456,
+#         "",
+#         "1234567890123456",
+#         None,
+#         True
+#     ]
+#     for i in account_numbers:
+#         print(get_mask_account(i))
+#     for i in account_numbers:
+#         print(get_mask_card_number(i))
--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,10 @@ import logging
 
 
 logging.basicConfig(
-    filename = '../logs/utils.log',
-    filemode = 'w',
-    format = '%(asctime)s:%(module)s:%(levelname)s:%(message)s',
-    level = logging.DEBUG
+    filename="../logs/utils.log",
+    filemode="w",
+    format="%(asctime)s:%(module)s:%(levelname)s:%(message)s",
+    level=logging.DEBUG,
 )
 
 logger = logging.getLogger()
@@ -21,30 +21,30 @@ def get_transactions_from_file(file_path: str) -> list:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей
     с данными о финансовых транзакциях. Если файл пустой, содержит не список или не найден,
      функция возвращает пустой список."""
-    logger.info('get_transactions_from_file started')
+    logger.info("get_transactions_from_file started")
     transactions_list = []
     try:
         with open(file_path, "r", encoding="utf-8") as transactions_list_file:
             try:
                 transactions_list = json.load(transactions_list_file)
-                logger.info(f'Receiving the transaction list from {file_path}')
+                logger.info(f"Receiving the transaction list from {file_path}")
             except json.JSONDecodeError:
-                logger.error(f'The transaction list received from {file_path} is incorrect')
+                logger.error(f"The transaction list received from {file_path} is incorrect")
                 print("Некорректный список транзакций")
-                logger.info('get_transactions_from_file ended')
+                logger.info("get_transactions_from_file ended")
                 return transactions_list
 
     except FileNotFoundError:
-        logger.error(f'{file_path} is not found')
+        logger.error(f"{file_path} is not found")
         print("Файл не найден")
-        logger.info('get_transactions_from_file ended')
+        logger.info("get_transactions_from_file ended")
         return transactions_list
 
-    logger.info('get_transactions_from_file ended')
+    logger.info("get_transactions_from_file ended")
     return transactions_list
 
 
 # Тестовый блок, для запуска раскомментировать
-if __name__ == '__main__':
-    result = get_transactions_from_file('../data/test_operations_empty.json')
-    print(result)
+# if __name__ == '__main__':
+#     result = get_transactions_from_file('../data/test_operations_empty.json')
+#     print(result)
