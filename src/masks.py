13911bdfb1d179c524d4b4bd4b3041a292e29d46
Alex-Stas <alexander.stashchuk@gmail.com>
2025-02-12 00:44:17 +0300

    Logging for utils is added

--- src/masks.py
+++ src/masks.py
@@ -16,3 +16,25 @@ def get_mask_account(full_account_number: int) -> str:
     account_number_string = str(full_account_number)
     masked_account_number = f"**{account_number_string[-4:]}"
     return masked_account_number
+
+
+if __name__ == "__main__":
+    account_numbers = [
+        1234567812345678,
+        10001111222233334444,
+        12345678901234567890,
+        1,
+        12,
+        123,
+        1234,
+        12345,
+        123456,
+        "",
+        "1234567890123456",
+        None,
+        True
+    ]
+    for i in account_numbers:
+        print(get_mask_account(i))
+    for i in account_numbers:
+        print(get_mask_card_number(i))
